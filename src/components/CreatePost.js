83786d847c06a6c22ecc0f855c37c97c0edfbf59
Ryotaro <ryotaro20250303@gmail.com>
2025-10-15 15:11:42 +0900

    Finish blog-app

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./CreatePost.css";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const CreatePost = () => {
+const CreatePost = ({ isAuth }) => {
+  const navigate = useNavigate();
   const [title, setTitle] = useState();
   const [postText, setPostText] = useState();
 
-  const createPost = () => {
-    console.log(title);
-    console.log(postText);
-  }
+  const createPost = async () => {
+    await addDoc(collection(db, "posts"), {
+      title: title,
+      postsText: postText,
+      author: {
+        username: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      },
+    });
+    navigate("/");
+  };
+
+  // 権限無い場合はログインにリダイレクト
+  useEffect(() => {
+    if(!isAuth) {
+      navigate("/login")
+    }
+  }, []);
 
   return (
     <div className="createPostPage">
@@ -16,13 +35,22 @@ const CreatePost = () => {
         <h1>記事を投稿する</h1>
         <div className="inputPost">
           <div>タイトル</div>
-          <input type="text" placeholder="タイトルを記入" onChange={(e) => setTitle(e.target.value)} />
+          <input
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>投稿</div>
-          <textarea placeholder="投稿内容を記入" onChange={(e) => setPostText(e.target.value)}></textarea>
+          <textarea
+            placeholder="投稿内容を記入"
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
-        <button className="postButton" onClick={createPost}>投稿する</button>
+        <button className="postButton" onClick={createPost}>
+          投稿する
+        </button>
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { auth, db } from "../firebase";
+import "./Home.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [postList, setPostList] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ doc })));
+      // console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); //dataメソッドを使うとデータの取り出しができる
+    };
+    getPosts();
+  }, []);
+
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id)); // doc関数を使ってpostsコレクションの中のidのポスト
+    window.location.href = "/"; //ホームにリダイレクト
+  };
+
+  return (
+    <div className="homePage">
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              {post.author.id === auth.currentUser?.uid && (
+                <button onClick={() => handleDelete(post.id)}>削除</button>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
